91bd6738ab867e72284df9cbe3efb24760015bea
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2023-11-13 09:40:45 -0500

    Update wordsearchii.cpp

--- wordsearchii.cpp
+++ wordsearchii.cpp
@@ -2,28 +2,21 @@
 #include <vector>
 #include <string>
 #include <unordered_set>
-// start
+
 using namespace std;
 
 class TrieNode {
 public:
     vector<TrieNode*> children;
     bool isEndOfWord;
-    TrieNode() : children(26), isEndOfWord(false) {}
-    ~TrieNode() {
-        for (auto child : children) {
-            delete child;
-        }
-    }
+    TrieNode() : children(26, nullptr), isEndOfWord(false) {}
 };
 
 class Trie {
 public:
     TrieNode* root;
     Trie() : root(new TrieNode()) {}
-    ~Trie() {
-        delete root;
-    }
+
     void insert(string word) {
         TrieNode* node = root;
         for (char c : word) {
@@ -36,7 +29,6 @@ public:
     }
 };
 
-
 class Solution {
 public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
@@ -44,36 +36,44 @@ public:
         for (string word : words) {
             trie.insert(word);
         }
-        unordered_set<string> result;
-        int m = board.size();
-        int n = board[0].size();
+
+        vector<string> result;
+        int m = board.size(), n = board[0].size();
         vector<vector<bool>> visited(m, vector<bool>(n, false));
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 dfs(board, visited, "", i, j, trie.root, result);
             }
         }
-        return vector<string>(result.begin(), result.end());
+
+        return result;
     }
+
 private:
-    void dfs(vector<vector<char>>& board, vector<vector<bool>>& visited, string word, int i, int j, TrieNode* node, unordered_set<string>& result) {
+    void dfs(vector<vector<char>>& board, vector<vector<bool>>& visited, string word, int i, int j, TrieNode* node, vector<string>& result) {
         if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || visited[i][j]) {
             return;
         }
+
         char c = board[i][j];
         if (!node->children[c - 'a']) {
             return;
         }
+
+        visited[i][j] = true;
         node = node->children[c - 'a'];
         word += c;
         if (node->isEndOfWord) {
-            result.insert(word);
+            result.push_back(word);
+            node->isEndOfWord = false; // avoid duplicates
         }
-        visited[i][j] = true;
+
         dfs(board, visited, word, i + 1, j, node, result);
         dfs(board, visited, word, i - 1, j, node, result);
         dfs(board, visited, word, i, j + 1, node, result);
         dfs(board, visited, word, i, j - 1, node, result);
+
         visited[i][j] = false;
     }
 };
