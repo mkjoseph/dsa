fa0356f086ca637adf419fc8272cc56788135171
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2023-11-14 09:18:35 -0500

    Update wordsearchii.cpp

--- wordsearchii.cpp
+++ wordsearchii.cpp
@@ -1,81 +1,26 @@
-
-#include <vector>
+#include <iostream>
 #include <string>
-#include <unordered_set>
 
-using namespace std;
+int main() {
+    std::string date, time, office, name;
 
-class TrieNode {
-public:
-    vector<TrieNode*> children;
-    bool isEndOfWord;
-    TrieNode() : children(26, nullptr), isEndOfWord(false) {}
-};
-// creating a trie for the words in the dictionary
+    std::cout << "Enter the date: ";
+    std::getline(std::cin, date);
 
+    std::cout << "Enter the time: ";
+    std::getline(std::cin, time);
 
-class Trie {
-public:
-    TrieNode* root;
-    Trie() : root(new TrieNode()) {}
+    std::cout << "Enter the office: ";
+    std::getline(std::cin, office);
 
-    void insert(string word) {
-        TrieNode* node = root;
-        for (char c : word) {
-            if (!node->children[c - 'a']) {
-                node->children[c - 'a'] = new TrieNode();
-            }
-            node = node->children[c - 'a'];
-        }
-        node->isEndOfWord = true;
-    }
-};
+    std::cout << "Enter the name: ";
+    std::getline(std::cin, name);
 
-class Solution {
-public:
-    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        Trie trie;
-        for (string word : words) {
-            trie.insert(word);
-        }
+    std::cout << "Appointment details:\n";
+    std::cout << "Date: " << date << "\n";
+    std::cout << "Time: " << time << "\n";
+    std::cout << "Office: " << office << "\n";
+    std::cout << "Name: " << name << "\n";
 
-        vector<string> result;
-        int m = board.size(), n = board[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                dfs(board, visited, "", i, j, trie.root, result);
-            }
-        }
-
-        return result;
-    }
-
-private:
-    void dfs(vector<vector<char>>& board, vector<vector<bool>>& visited, string word, int i, int j, TrieNode* node, vector<string>& result) {
-        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || visited[i][j]) {
-            return;
-        }
-
-        char c = board[i][j];
-        if (!node->children[c - 'a']) {
-            return;
-        }
-
-        visited[i][j] = true;
-        node = node->children[c - 'a'];
-        word += c;
-        if (node->isEndOfWord) {
-            result.push_back(word);
-            node->isEndOfWord = false; // avoid duplicates
-        }
-
-        dfs(board, visited, word, i + 1, j, node, result);
-        dfs(board, visited, word, i - 1, j, node, result);
-        dfs(board, visited, word, i, j + 1, node, result);
-        dfs(board, visited, word, i, j - 1, node, result);
-
-        visited[i][j] = false;
-    }
-};
+    return 0;
+}
