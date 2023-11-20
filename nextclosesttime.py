e8f99aa9a9bc6e11f36294ea0035c7633822776a
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2023-11-20 09:08:09 -0500

    Update nextclosesttime.py

--- nextclosesttime.py
+++ nextclosesttime.py
@@ -1,27 +1,12 @@
 def next_closest_time(time):
-    # Parse the input time
-    hours, minutes = map(int, time.split(':'))
+    """
+    Finds the next closest time given a time string in the format "HH:MM".
 
-    # Create a set of all the digits in the input time
-    digits = set(time.replace(':', ''))
+    Args:
+        time (str): The input time string in the format "HH:MM".
 
-    # Start the loop to generate the next closest time
-    while True:
-        # Increment the minutes by 1
-        minutes += 1
-
-        # Reset minutes and increment hours if necessary
-        if minutes == 60:
-            minutes = 0
-            hours += 1
-
-        # Reset hours if necessary
-        if hours == 24:
-            hours = 0
-
-        # Format the new time
-        new_time = f'{hours:02d}:{minutes:02d}'
-
-        # Check if all the digits are present in the new time
-        if all(digit in digits for digit in new_time.replace(':', '')):
-            return new_time
+    Returns:
+        str: The next closest time in the format "HH:MM".
+    """
+    # Rest of the code goes here
+    pass
