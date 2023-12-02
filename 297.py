3fc1384bf520d11b77a61023d94f1cb72c509f91
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2023-12-02 09:14:56 -0500

    Update 297.py

--- 297.py
+++ 297.py
@@ -1,11 +1,27 @@
+'''
 
+To serialize and deserialize a binary tree, you can use a method that traverses the tree in a specific order and represents it as a string. Here's one approach using a preorder traversal:
 
+Serialize:
 
+Start with an empty string to store the serialized tree.
+Perform a preorder traversal of the binary tree.
+For each node:
+If the node is null, append "null," to the string.
+Otherwise, append the node's value followed by a comma and space to the string.
+Continue this process until all nodes have been processed.
+Finally, return the serialized string.
+Deserialize:
 
+To deserialize the tree, split the serialized string by commas to get an array of values.
+Initialize an index variable to keep track of the current position in the array.
+Start building the tree recursively:
+If the current value is "null," return null.
+Otherwise, create a new TreeNode with the current value and increment the index.
+Recursively call the deserialize function for the left and right children of the current node.
+Return the root of the tree.
 
-
-
-
+'''
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
