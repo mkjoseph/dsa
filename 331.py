97e3ca2fba65a6973b7fa071315e86a78f6f6386
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2023-12-07 10:05:22 -0500

    Update 331.py

--- 331.py
+++ 331.py
@@ -21,3 +21,38 @@ def isValidSerialization(preorder: str) -> bool:
             stack.append('#')
 
     return len(stack) == 1 and stack[0] == '#'
+
+
+# Pass 2 
+class Solution(object):
+    def isValidSerialization(self, preorder):
+        """
+        :type preorder: str
+        :rtype: bool
+        """
+        # remember how many empty slots we have
+        # non-null nodes occupy one slot but create two new slots
+        # null nodes occupy one slot
+        
+        p = preorder.split(',')
+        
+        #initially we have one empty slot to put the root in it
+        slot = 1
+        for node in p:
+            
+            # no empty slot to put the current node
+            if slot == 0:
+                return False
+                
+            # a null node?
+            if node == '#':
+                # ocuppy slot
+                slot -= 1
+            else:
+                # create new slot
+                slot += 1
+        
+        #we don't allow empty slots at the end
+        return slot==0
+    
+# Pass 3
