04cc4ca4931a72659515e176642b09b2f4f948f1
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2023-12-08 09:53:58 -0500

    Update 989.py

--- 989.py
+++ 989.py
@@ -17,3 +17,12 @@ def addToArrayForm(num, k):
 
 # Pass 2 ------------------- 
 
+def addToArrayForm(num, k):
+    num = int(''.join(map(str, num)))  # Convert num array to an integer
+    num += k  # Add k to the integer value of num
+    return list(map(int, str(num)))  # Convert the sum back to an array of digits
+
+# Test cases
+print(addToArrayForm([1,2,0,0], 34))  # Output: [1,2,3,4]
+print(addToArrayForm([2,7,4], 181))  # Output: [4,5,5]
+print(addToArrayForm([2,1,5], 806))  # Output: [1,0,2,1]
