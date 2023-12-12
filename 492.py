eeffb0ff9e2ef987be710f77f2bc0720ee330a17
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2023-12-12 10:03:19 -0500

    Update 492.py

--- 492.py
+++ 492.py
@@ -1,8 +1,26 @@
 import math
 
 def constructRectangle(area):
+    """
+    Constructs a rectangle with the given area.
+
+    Parameters:
+    area (int): The area of the rectangle.
+
+    Returns:
+    list: A list containing the length and width of the rectangle.
+    """
     width = int(math.sqrt(area))
     while area % width != 0:
         width -= 1
     length = area // width
     return [length, width]
+
+
+#pass 2
+
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+        for l in range(int(area**0.5), 0, -1):            
+            if area % l == 0: 
+                return [area // l, l]
