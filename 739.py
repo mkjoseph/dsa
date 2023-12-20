1c52d4e831f991f4ae184f1de4a8fa6b115fbf62
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2023-12-20 11:08:54 -0500

    Update 739.py

--- 739.py
+++ 739.py
@@ -14,13 +14,22 @@ def dailyTemperatures(temperatures):
 
 #Pass 2 
 
- def dailyTemperatures(self, T):
+def dailyTemperatures(self, T):
+    """
+    Calculates the number of days you have to wait until a warmer temperature is found for each day.
+
+    Args:
+        T (List[int]): A list of daily temperatures.
+
+    Returns:
+        List[int]: A list of integers representing the number of days you have to wait until a warmer temperature is found for each day.
+    """
     ans = [0] * len(T)
     stack = []
     for i, t in enumerate(T):
-      while stack and T[stack[-1]] < t:
-        cur = stack.pop()
-        ans[cur] = i - cur
-      stack.append(i)
+        while stack and T[stack[-1]] < t:
+            cur = stack.pop()
+            ans[cur] = i - cur
+        stack.append(i)
 
     return ans
