bdbde666ff381756278b67ee427fe3f47e1ab952
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2023-12-28 22:21:51 -0500

    Create 830.py

---
+++ 830.py
@@ -0,0 +1,11 @@
+    def largeGroupPositions(self, S):
+        i, j, N = 0, 0, len(S)
+        res = []
+        while i < N:
+            while j < N and S[j] == S[i]: j += 1
+            if j - i >= 3: res.append([i, j - 1])
+            i = j
+        return res
+
+# pass 2
+
