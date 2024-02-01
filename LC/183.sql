aaff5287f092117efab86fc3ef8dc13140b92e57
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2024-02-01 10:33:59 -0500

    Create 183.sql

---
+++ LC/183.sql
@@ -0,0 +1,12 @@
+SELECT name AS Customers
+FROM Customers
+WHERE id NOT IN (
+SELECT customerId FROM Orders
+)
+
+
+/*
+This query:
+Uses a subquery to get all customerIds that have ordered from the Orders table.
+Filters the Customers table to only return rows where the id is NOT IN the list of customerIds from the subquery.
+This returns customers that don't have a match in the Orders table, meaning they have never ordered. */
