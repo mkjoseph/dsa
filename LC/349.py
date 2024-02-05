b2469780ca49bdfbebb0db7c5e2a62c9bd838b61
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2024-02-05 14:29:27 +0000

    update 349

--- LC/349.py
+++ LC/349.py
@@ -11,3 +11,62 @@ def intersection(self, nums1, nums2):
 
 
 # pass 2
+class Solution(object):
+def intersection(self, nums1, nums2):
+    """
+    :type nums1: List[int]
+    :type nums2: List[int]
+    :rtype: List[int]
+    """
+    res = []
+    for i in nums1:
+        if i not in res and i in nums2:
+            res.append(i)
+
+    return res
+
+# pass 3
+
+class Solution(object):
+def intersection(self, nums1, nums2):
+    """
+    :type nums1: List[int]
+    :type nums2: List[int]
+    :rtype: List[int]
+    """
+    res = []
+    map = {}
+    for i in nums1:
+        map[i] = map[i]+1 if i in map else 1
+    for j in nums2:
+        if j in map and map[j] > 0:
+            res.append(j)
+            map[j] = 0
+    
+    return res
+
+# pass 4
+
+class Solution(object):
+def intersection(self, nums1, nums2):
+    """
+    :type nums1: List[int]
+    :type nums2: List[int]
+    :rtype: List[int]
+    """
+    res = []
+    nums1.sort()
+    nums2.sort()
+    i = j = 0
+    while (i < len(nums1) and j < len(nums2)):
+        if nums1[i] > nums2[j]:
+            j += 1
+        elif nums1[i] < nums2[j]:
+            i += 1
+        else:
+            if not (len(res) and nums1[i] == res[len(res)-1]):
+                res.append(nums1[i])
+            i += 1
+            j += 1
+
+    return res
