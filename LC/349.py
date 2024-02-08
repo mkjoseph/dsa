d4239a37231aab63c8973cb386e42cc8f2a79a65
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2024-02-08 04:29:54 +0000

    add comment

--- LC/349.py
+++ LC/349.py
@@ -1,13 +1,14 @@
 # pass 1
 class Solution(object):
-def intersection(self, nums1, nums2):
-    """
-  
-    :type nums1: List[int]
-    :type nums2: List[int]
-    :rtype: List[int]
-    """
-    return list(set(nums1) & set(nums2))
+  # Find the intersection of two lists
+  def intersection(self, nums1, nums2):
+      """
+      :type nums1: List[int]
+      :type nums2: List[int]
+      :rtype: List[int]
+      """
+      # Use set intersection to find common elements
+      return list(set(nums1) & set(nums2))
 
 
 # pass 2
